0539a2d3bd7846ce6e2eb396ca427810fb73a64a
jhall71 <46503029+jhall71@users.noreply.github.com>
2020-11-10 11:30:20 -0800

    Update pywebapp.py

--- pywebapp.py
+++ pywebapp.py
@@ -14,7 +14,7 @@ class Default:
         return """<html>
         <title>PyWebApp</title>
         <body bgcolor="gray">
-        <h1>Hello COMP4964!</h1>
+        <h1>Jared Hall A00714173</h1>
         <p>Available API methods:</p>
         <ul>
         <li><a href="/api/v1/test">/api/v1/test</a></li>
